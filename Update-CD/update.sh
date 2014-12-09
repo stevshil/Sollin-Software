e2d59111115b6abe64905ca77c66365cab35ca6e
Steve Shilling <steve@therapypages.com>
2014-12-09 22:30:18 +0000

    Modified script to enable F14 and F20 updates by checking version as F14 uses /media not /run/media/sol1004

--- Update-CD/update.sh
+++ Update-CD/update.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 kill -9 `ps -ef | grep LCDClient | grep -v grep | awk '{print $2}'`
 
-if [[ -e /run/media/sol1004/Update/LCDClient.jar ]]
+CDDIR='/run/media/sol1004'
+
+if [[ -e /etc/redhat-release ]]
 then
-	cp /run/media/sol1004/Update/LCDClient.jar /home/sol1004/LCDClient.jar
+	if grep 'Fedora release 14' /etc/redhat-release >/dev/null 2>&1
+	then
+		CDDIR=/media
+	fi
 fi
 
-if [[ -d /run/media/sol1004/Update/lib ]]
+if [[ -e $CDDIR/Update/LCDClient.jar ]]
 then
-	cp -r /run/media/sol1004/Update/lib/* /home/sol1004/lib
+	cp $CDDIR/Update/LCDClient.jar /home/sol1004/LCDClient.jar
 fi
 
-if [[ -d /run/media/sol1004/Update/conf ]]
+if [[ -d $CDDIR/Update/lib ]]
 then
-	cp -r /run/media/sol1004/Update/conf/* /home/sol1004/conf
+	cp -r $CDDIR/Update/lib/* /home/sol1004/lib
 fi
 
-if [[ -d /run/media/sol1004/Update/tracks ]]
+if [[ -d $CDDIR/Update/conf ]]
 then
-	cp -r /run/media/sol1004/Update/tracks/* /home/sol1004/tracks
+	cp -r $CDDIR/Update/conf/* /home/sol1004/conf
 fi
 
-if [[ -d /run/media/sol1004/Update/playlists ]]
+if [[ -d $CDDIR/Update/tracks ]]
 then
-	cp -r /run/media/sol1004/Update/playlists/* /home/sol1004/playlists
+	cp -r $CDDIR/Update/tracks/* /home/sol1004/tracks
+fi
+
+if [[ -d $CDDIR/Update/playlists ]]
+then
+	cp -r $CDDIR/Update/playlists/* /home/sol1004/playlists
 fi
 
 chown -R sol1004:sol1004 /home/sol1004
